888078efe11d9ce4e7a7f01206dcb84c76d06638
Call Center <rodlemus30@gmail.com>
2024-03-22 15:28:23 -0600

    paths relativos agregados en el index v3 test dinamic url

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
-const cinemasSelectElement = document.getElementById("availableCinemas");
-
-console.log(cinemasSelectElement.options);
+const generateUrlByEnvironment = () => {
+  const cinemasSelectElement = document.getElementsByClassName("appUrl");
+  const isLocalhost =
+    window.location.host.match("localhost") ||
+    window.location.host.match("127.*");
+  Array.from(cinemasSelectElement).forEach((navbarUrlElement) => {
+    if (isLocalhost) {
+      console.log(navbarUrlElement.href);
+      console.log(String(navbarUrlElement.href).indexOf("app"));
+    }
+  });
+};
+generateUrlByEnvironment();
